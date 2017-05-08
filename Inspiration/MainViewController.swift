e16a7d0d7baffec735b530b23fbbf9874d148881
Konstantin Khokhlov <sqb@mail.ru>
2017-05-08 16:58:40 +0400

    Start

---
+++ Inspiration/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  Inspiration
+//
+//  Created by Konstantin Khokhlov on 08.05.17.
+//  Copyright © 2017 Konstantin Khokhlov. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
