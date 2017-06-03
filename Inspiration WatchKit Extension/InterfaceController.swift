ab54f5d0ea646e36d05168699bb7a4c7a3f91082
Konstantin Khokhlov <sqb@mail.ru>
2017-06-03 09:41:55 +0400

    Watch Kit Target added

---
+++ Inspiration WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,30 @@
+//
+//  InterfaceController.swift
+//  Inspiration WatchKit Extension
+//
+//  Created by Konstantin Khokhlov on 03.06.17.
+//  Copyright © 2017 Konstantin Khokhlov. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
